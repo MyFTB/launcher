56a76af3c713e77fbc05196b91463f6c0c2632a0
Dogboy21 <dogboy2103@gmail.com>
2019-04-15 01:49:39 +0200

    UI Webpack Setup

---
+++ src/main/ui/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, '../resources/webroot')
+    }
+};
