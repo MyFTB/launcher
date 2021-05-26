71bff2051e3cd52580e732bd09d21af79890ba63
Dogboy21 <dogboy2103@gmail.com>
2021-05-26 21:43:53 +0200

    [skip-ci] Lizenzheader auf das Jahr 2021 aktualisiert

--- src/main/java/de/myftb/launcher/models/modpacks/ModpackManifest.java
+++ src/main/java/de/myftb/launcher/models/modpacks/ModpackManifest.java
@@ -1,6 +1,6 @@
 /*
  * MyFTBLauncher
- * Copyright (C) 2020 MyFTB <https://myftb.de>
+ * Copyright (C) 2021 MyFTB <https://myftb.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package de.myftb.launcher.models.modpacks;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
