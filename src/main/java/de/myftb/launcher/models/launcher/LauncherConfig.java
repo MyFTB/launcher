62152478cc414775f47af7854acdbbb0bd162680
Dogboy21 <info@dogboy.io>
2024-03-03 12:30:42 +0100

    Excuse me, wir haben 2024

--- src/main/java/de/myftb/launcher/models/launcher/LauncherConfig.java
+++ src/main/java/de/myftb/launcher/models/launcher/LauncherConfig.java
@@ -1,6 +1,6 @@
 /*
  * MyFTBLauncher
- * Copyright (C) 2023 MyFTB <https://myftb.de>
+ * Copyright (C) 2024 MyFTB <https://myftb.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
